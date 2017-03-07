dcdecbd7f23449d9f8414b22e24018f43e60fde3
Ray Meibaum <rmeibaum@me.com>
2017-03-07 14:43:25 -0500

    add animate.css for block animations

--- js/main.js
+++ js/main.js
@@ -1,16 +1,31 @@
 'use-strict';
-const Sequence = {
+const GameData = {
+  sequence: [],
+  extendSequence() {
+    this.sequence.push(Math.floor(Math.random() * (4)));
+    return this.sequence;
+  },
+  deleteSequence() {
+    this.sequence = [];
+  },
 
 };
 
 const Controller = {
-
+  onClickStartButton() {
+    Presenter.runSequence(GameData.extendSequence());
+  }
 }
 
 const Presenter = {
+  runSequence(sequence) {
+    console.log(sequence);
+  },
+  animateSquare(index) {
 
+  }
 }
 
 $(function() {
-
+  $('button#start').on('click', Controller.onClickStartButton);
 })
