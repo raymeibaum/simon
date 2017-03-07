2e6196330727c83f9fab7fdd66b23ac07f035f01
Ray Meibaum <rmeibaum@me.com>
2017-03-07 16:03:44 -0500

    removed animate.css animations and implemented opacity-based cues

--- js/main.js
+++ js/main.js
@@ -20,9 +20,21 @@ const Controller = {
 const Presenter = {
   runSequence(sequence) {
     console.log(sequence);
+    let timer = 0;
+    sequence.forEach(function(element) {
+      window.setTimeout(function() {
+        Presenter.animateSquare(Presenter.getBoxByIndex(element));
+      }, timer += 1000);
+    });
   },
-  animateSquare(index) {
-
+  animateSquare($square) {
+    $square.css('opacity', '0.4');
+    window.setTimeout(function() {
+      $square.css('opacity', '1.0');
+    }, 666);
+  },
+  getBoxByIndex(index) {
+    return $(`div.box[data-index=${index}]`);
   }
 }
 
